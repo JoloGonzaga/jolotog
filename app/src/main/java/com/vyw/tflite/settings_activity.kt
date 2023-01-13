195b306489cf667d6ac283422995155dff15033e
jologonzaga <gonzagalloyd06@gmail.com>
2023-01-13 15:24:28 +0800

    final

--- app/src/main/java/com/vyw/tflite/settings_activity.kt
+++ app/src/main/java/com/vyw/tflite/settings_activity.kt
@@ -1,12 +1,21 @@
 package com.vyw.tflite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.vyw.tflite.R
 
 class settings_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
     }
+        fun exit2_btnclick(view: View) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
 }
