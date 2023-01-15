a6d4e6d0d8d161f9e8bf05d88b49b338dd033b4d
jologonzaga <gonzagalloyd06@gmail.com>
2023-01-15 19:48:54 +0800

    final wih splash and updated about

--- app/src/main/java/com/vyw/tflite/splash.kt
+++ app/src/main/java/com/vyw/tflite/splash.kt
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import com.vyw.tflite.R
 
 class splash : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
 
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
